6e30007aead830b50cf0f7c6053f5b9044a9ac6b
Apilv <38555903+Apilv@users.noreply.github.com>
2020-07-31 19:23:23 +0300

    minor update

--- index.php
+++ index.php
@@ -14,10 +14,16 @@ if (!$conn) {
 }
 
 #---------------SELECTING DATA FROM DB-------------------
+
 $sql = "SELECT id, name, project FROM employees";
 $result = mysqli_query($conn, $sql);
 
+mysqli_close($conn);
 ?>
+
+
+
+<!---------------HTML------------------->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,20 +32,23 @@ $result = mysqli_query($conn, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Project</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-    <!---------------TABLE------------------>
     <form>
-        <input type="button" action="get" value="Projects">
-        <input type="button" action="get" value="Employees">
+        <input type="button" action="get" name="projects" value="Projects">
+        <input type="button" action="get" name="emploees" value="Employees">
     </form>
+
+    <!---------------TABLE------------------>
+    
     <?php
     echo ('<table>
-         <th>ID</thead>
-         <th>Name</th>
-         <th>Project</th>');
+        <th>ID</thead>
+        <th>Name</th>
+        <th>Project</th>');
 
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
