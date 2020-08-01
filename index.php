b7fecc84a068da7f433e62b7934be914169ac673
Apilv <38555903+Apilv@users.noreply.github.com>
2020-08-01 18:22:06 +0300

    dead link removed

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ include 'logic.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Project</title>
     <link rel="stylesheet" href="style.css">
-
-    <script type="text/javascript">
-        document.getElementsById("#projects").click();
-    </script>
 </head>
 
 <body>
